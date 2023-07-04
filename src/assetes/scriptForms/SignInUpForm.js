b802ed9e8c8287ec1ef2ad21be7857d7ad24aecb
choaybx18 <chouaib2022x18@gmail.com>
2023-07-04 23:10:05 +0100

    second commet
    v2

--- src/assetes/scriptForms/SignInUpForm.js
+++ src/assetes/scriptForms/SignInUpForm.js
@@ -34,34 +34,37 @@ function SignInUpForm() {
     <div className={`container ${isSignUpActive ? 'right-panel-active' : ''}`}>
       <div className="form-container sign-up-container">
         <form action="#">
-        <h1>Create Account</h1>
+       
+        <h1><i className="col" >Create Account</i></h1>
           <br></br>
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
-          <button>Sign Up</button>
+          
+            <button  className="btnsin" >Sign Up </button>
+         
           <br></br>
           <span>or use social media for registration</span>
           <div className="social-container">
-            <a href="#" className="social"><i className="fab fa-facebook-f" style={{ color: 'white' }}></i></a>
-            <a href="#" className="social"><i className="fab fa-google-plus-g" style={{ color: 'white' }}></i></a>
-            <a href="#" className="social"><i className="fab fa-twitter" style={{ color: 'white' }}></i></a>
+            <a href="#" className="social" style={{ backgroundColor: '#3b5998 ' }}><i className="fab fa-facebook-f" style={{ color: 'white' }}></i></a>
+            <a href="#" className="social" style={{ backgroundColor: '#db4a39' }}><i className="fab fa-google-plus-g" style={{ color: 'white' }}></i></a>
+            <a href="#" className="social"style={{ backgroundColor: '#00acee' }}><i className="fab fa-twitter" style={{ color: 'white' }}></i></a>
           </div>
         </form>
       </div>
       <div className="form-container sign-in-container">
         <form onSubmit={handleLogin}>
-          <h1>Log in</h1>
+          <h1><i className="col2" >Log in</i></h1>
           <br></br>
           <input type="email" name="email" placeholder="Email" />
           <input type="password" name="password" placeholder="Password" />
-          <button type="submit">Login</button>
+          <button type="submit" className="btnlog" >Log in</button>
           <br></br>
           <span>or use social media for login</span>
           <div className="social-container">
-            <a href="#" className="social"><i className="fab fa-facebook-f" style={{ color: 'white' }}></i></a>
-            <a href="#" className="social"><i className="fab fa-google-plus-g" style={{ color: 'white' }}></i></a>
-            <a href="#" className="social"><i className="fab fa-twitter" style={{ color: 'white' }}></i></a>
+            <a href="#" className="social" style={{ backgroundColor: '#3b5998 ' }}><i className="fab fa-facebook-f" style={{ color: 'white' }}></i></a>
+            <a href="#" className="social" style={{ backgroundColor: '#db4a39' }}><i className="fab fa-google-plus-g" style={{ color: 'white' }}></i></a>
+            <a href="#" className="social"style={{ backgroundColor: '#00acee' }}><i className="fab fa-twitter" style={{ color: 'white' }}></i></a>
           </div>
         
         </form>
@@ -72,13 +75,13 @@ function SignInUpForm() {
           <div className="overlay-panel overlay-left">
             <h1>Welcome Back!</h1>
             <p>To keep connected with us, please login with your personal info</p>
-            <button className="ghost" onClick={handleSignInClick}>Log In</button>
+            <button className="btnghost" onClick={handleSignInClick}>Log In</button>
           </div>
           <div className="overlay-panel overlay-right">
             <h1>Hello, Friend!</h1>
             <p>Enter your personal details and start your journey with us</p>
             <br></br>
-            <button className="ghost" onClick={handleSignUpClick}>Sign Up</button>
+            <button className="btnghost" onClick={handleSignUpClick}>Sign Up</button>
           </div>
         </div>
       </div>
